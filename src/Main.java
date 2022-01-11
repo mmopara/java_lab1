3d819ac66bb287b9a0ad690ae8a22ab60b57e8a6
mmopara <mmopara@gmail.com>
2022-01-11 10:50:56 +0100

    cw 6

--- src/Main.java
+++ src/Main.java
@@ -10,5 +10,18 @@ public class Main {
         przypisać do niej wartości i wyświetlić */
 
 
+        double [] tab = new double[10];
+
+        tab[1] = 1;
+        tab[2] = 2;
+        tab[3] = 3;
+        tab[4] = 4;
+        tab[5] = 5;
+
+        System.out.println("Pierwsza pozycja w tabeli to: " + tab[1]);
+        System.out.println("Druga pozycja w tabeli to: " + tab[2]);
+        System.out.println("Trzecia pozycja w tabeli to: " + tab[3]);
+        System.out.println("Czwarta pozycja w tabeli to: " + tab[4]);
+        System.out.println("Piąta pozycja w tabeli to: " + tab[5]);
     }
 }
