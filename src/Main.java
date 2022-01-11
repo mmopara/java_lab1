6367bb70903b3c6ae488eaf9b6b1d2dae070743a
mmopara <mmopara@gmail.com>
2022-01-11 09:53:59 +0100

    cw 1

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,9 @@ public class Main {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
 
+        System.out.println("Dzień dobry Panie Krzysztofie,");
+        System.out.printf("Na imię mam %s, a na nazwisko %s.", "Maciej", "Opara");
+        System.out.print(" Życzę miłego dnia.");
+        //System.out.print();
     }
 }
