e76c3b6865455acb4177a0a89f961520856c7741
mmopara <mmopara@gmail.com>
2022-01-15 08:59:56 +0100

    cw 4

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,21 @@ public class Main {
 
         /* ćw.4 */
 
-        /* zad.1 proszę dodać kilka złożonych operacji uzywając kilku operatorów arytmetycznych i logicznych */
+        /* zad.1 Proszę dodać kilka złożonych operacji uzywając kilku operatorów arytmetycznych i logicznych */
+
+        boolean a = true;
+        boolean b = true;
+        boolean c = false;
+
+        System.out.println("(a!=b)" + (a != b));
+        System.out.println("(a&&b)" + (a && b));
+        System.out.println("(b||c)" + (b || c));
 
 
+        int x = 3;
+        int y = 7;
+        int z = 14;
+        System.out.println("(x>z)" + (x > z));
+        System.out.println("((a&&b)||(x>x))" + ((a && b) || (x > x)));
     }
 }
